3549835fba37d841027a861cac9d729a207241fa
DuyNguyen <duyyb257946@gmail.com>
2025-02-13 13:52:25 +0700

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log("🚀 ~ io.on ~ socket:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("offer", (offer) => {
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("candidate", (candidate) => {
+    socket.broadcast.emit("candidate", candidate);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
